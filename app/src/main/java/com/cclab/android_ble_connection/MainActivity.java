9af2a48b590ee6bebcf1211a89d7683762bf376c
hoho <kumass2021@gmail.com>
2021-05-25 03:44:26 +0900

    init

---
+++ app/src/main/java/com/cclab/android_ble_connection/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cclab.android_ble_connection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
