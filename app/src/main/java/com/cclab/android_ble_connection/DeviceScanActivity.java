e697cccfeb1388557d6aff3813ac0abc0b901c83
kumass2020 <kumass2021@gmail.com>
2021-05-25 16:59:29 +0900

    document based modifying (working)

--- app/src/main/java/com/cclab/android_ble_connection/DeviceScanActivity.java
+++ app/src/main/java/com/cclab/android_ble_connection/DeviceScanActivity.java
@@ -1,7 +1,10 @@
 package com.cclab.android_ble_connection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
@@ -16,19 +19,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Activity for scanning and displaying available BLE devices.
+ */
+public class DeviceScanActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
     private final static int REQUEST_ENABLE_BT = 1;
+
+    private LeDeviceListAdapter mLeDeviceListAdapter;
+    private boolean mScanning;
+    private Handler handler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+    // ...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get permission
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        ActivityCompat.requestPermissions(DeviceScanActivity.this, permission_list,  1);
 
         // Use this check to determine whether BLE is supported on the device. Then
         // you can selectively disable BLE-related features.
@@ -50,39 +72,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Activity for scanning and displaying available BLE devices.
-     */
-    public class DeviceScanActivity extends ListActivity {
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothAdapter.stopLeScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
 
-        private BluetoothAdapter bluetoothAdapter;
-        private boolean mScanning;
-        private Handler handler;
-
-        // Stops scanning after 10 seconds.
-        private static final long SCAN_PERIOD = 10000;
-        // ...
-        private void scanLeDevice(final boolean enable) {
-            if (enable) {
-                // Stops scanning after a pre-defined scan period.
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        mScanning = false;
-                        bluetoothAdapter.stopLeScan(leScanCallback);
-                    }
-                }, SCAN_PERIOD);
-
-                mScanning = true;
-                bluetoothAdapter.startLeScan(leScanCallback);
-            } else {
-                mScanning = false;
-                bluetoothAdapter.stopLeScan(leScanCallback);
-            }
-            // ...
+            mScanning = true;
+            bluetoothAdapter.startLeScan(leScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothAdapter.stopLeScan(leScanCallback);
         }
         // ...
     }
+    // ...
 
     private LeDeviceListAdapter leDeviceListAdapter;
 
@@ -90,12 +99,25 @@ public class MainActivity extends AppCompatActivity {
     private class LeDeviceListAdapter extends BaseAdapter {
         private ArrayList<BluetoothDevice> mLeDevices;
         private LayoutInflater mInflator;
-        public DeviceScanActivity;
 
         public LeDeviceListAdapter() {
             super();
             mLeDevices = new ArrayList<BluetoothDevice>();
-            mInflator = DeviceScanActivity.this.getLayoutInflater();
+            mInflator = com.cclab.android_ble_connection.DeviceScanActivity.this.getLayoutInflater();
+        }
+
+        public void addDevice(BluetoothDevice device) {
+            if(!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
+            }
+        }
+
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+
+        public void clear() {
+            mLeDevices.clear();
         }
 
         @Override
@@ -105,17 +127,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Object getItem(int i) {
-            return null;
+            return mLeDevices.get(i);
         }
 
         @Override
         public long getItemId(int i) {
-            return 0;
+            return i;
         }
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = mInflator.inflate(R.layout.listitem_device, null);
+                viewHolder = new ViewHolder();
+                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            BluetoothDevice device = mLeDevices.get(i);
+            final String deviceName = device.getName();
+            if (deviceName != null && deviceName.length() > 0)
+                viewHolder.deviceName.setText(deviceName);
+            else
+                viewHolder.deviceName.setText(R.string.unknown_device);
+            viewHolder.deviceAddress.setText(device.getAddress());
+
+            return view;
         }
     }
 
@@ -134,5 +176,10 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             };
+
+    static class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
+    }
 
 }
